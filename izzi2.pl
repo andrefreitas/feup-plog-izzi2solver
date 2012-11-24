d1bb54c89424b73dd84ba1657b6c1fb5cc77b771
Andre Freitas <p.andrefreitas@gmail.com>
2012-11-24 12:28:25 +0000

    Já consegue verificar uma ligação entre peças

--- izzi2.pl
+++ izzi2.pl
@@ -49,6 +49,27 @@ getColor(Piece,Index,Color):-
 	).
 
 % Create Pieces List
-createListOfPieces(Pieces):-
+createPieces(Pieces):-
 	p1(P1),p2(P2),p3(P3),p4(P4),p5(P5),p6(P6),p7(P7),p8(P8),p9(P9),p10(P10),p11(P11),p12(P12),
 	Pieces=[P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12].
+
+% Get Piece from piece list
+getPiece(Pieces,Index,Piece):-
+	Pieces=[H|_],
+	Index=1,
+	Piece=H.
+
+getPiece(Pieces,Index,Piece):-
+	Index>1,
+	NewIndex is Index-1,
+	Pieces=[_|T],
+	getPiece(T,NewIndex,Piece).
+
+% Connection
+connect(Pieces,Connection):-
+	Connection=[Piece1Index,Piece2Index,P1ColorIndex,P2ColorIndex],
+	getPiece(Pieces,Piece1Index,Piece1),
+	getPiece(Pieces,Piece2Index,Piece2),
+	getColor(Piece1,P1ColorIndex,Piece1IndexColor),
+	getColor(Piece2,P2ColorIndex,Piece2IndexColor),
+	Piece1IndexColor=Piece2IndexColor.

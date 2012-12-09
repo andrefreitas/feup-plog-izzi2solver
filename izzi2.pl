4a91a772899cd220fdc9e933c7f4acd8a0292c44
rui goncalves <rui.nf1@gmail.com>
2012-12-09 12:25:06 +0000

    added shape with corner example

--- izzi2.pl
+++ izzi2.pl
@@ -138,7 +138,35 @@ connectionCorner(Connection):-
 		(I1#=3, I2#=4, O1#=1,O2#=0)
 	).
 
-shapeSimple(Connections):-
+shapeSimpleShash(Connections):-
+	createPieces(Pieces),
+	Connections=[C1,C2,C3,C4],
+
+	C1=[P1,P2,C1I1,C1I2,O1,O2],
+	C2=[P1,P3,C2I1,C2I2,O1,O3],
+	C3=[P3,P4,C3I1,C3I2,O3,O4],
+	C4=[P2,P4,C4I1,C4I2,O2,O4],
+
+	connectionSlashRight(C1),
+	connectionSlashLeft(C2),
+	connectionSlashRight(C3),
+	connectionSlashLeft(C4),
+
+	connect(Pieces,C1),
+	connect(Pieces,C2),
+	connect(Pieces,C3),
+	connect(Pieces,C4),
+
+	domain([P1,P2,P3,P4],1,12),
+	domain([C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2],1,4),
+	domain([O1,O2,O3,O4],0,1),
+
+	all_different([P1,P2,P3,P4]),
+	unique(Connections),
+	Sol=[P1,P2,P3,P4,C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2,O1,O2,O3,O4],
+	labeling([],Sol).
+
+shapeSimpleCorner(Connections):-
 	createPieces(Pieces),
 	Connections=[C1,C2],
 

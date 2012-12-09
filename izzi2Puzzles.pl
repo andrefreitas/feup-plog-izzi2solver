880b9570544a17fbdcc00f24e2ee8d6872d433e2
André Freitas <p.andrefreitas@gmail.com>
2012-12-09 20:12:38 +0000

    Reverted shape4corners

--- izzi2Puzzles.pl
+++ izzi2Puzzles.pl
@@ -2,36 +2,29 @@
 
 shape4Corners(Connections):-
 	createPieces(Pieces),
-	Connections=[C1,C2,C3,C4,C5,C6],
+	Connections=[C1,C2,C3,C4],
 
 	C1=[P1,P2,C1I1,C1I2,O1,O2],
 	C2=[P1,P3,C2I1,C2I2,O1,O3],
 	C3=[P2,P4,C3I1,C3I2,O2,O4],
 	C4=[P3,P4,C4I1,C4I2,O3,O4],
-	C5=[P5,P3,C5I1,C5I2,O5,O3],
-	C6=[P4,P5,C6I1,C6I2,O4,O5],
-
 	connectionCorner(C1),
 	connectionSlashLeft(C2),
 	connectionSlashRight(C3),
 	connectionCorner(C4),
-	connectionCorner(C5),
-	connectionCorner(C6),
 
 	connect(Pieces,C1),
 	connect(Pieces,C2),
 	connect(Pieces,C3),
 	connect(Pieces,C4),
-	connect(Pieces,C5),
-	connect(Pieces,C6),
 
-	domain([P1,P2,P3,P4,P5],1,12),
-	domain([C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2,C5I1,C5I2,C6I1,C6I2],1,4),
-	domain([O1,O2,O3,O4,O5],0,1),
+	domain([P1,P2,P3,P4],1,12),
+	domain([C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2],1,4),
+	domain([O1,O2,O3,O4],0,1),
 
-	all_different([P1,P2,P3,P4,P5]),
+	all_different([P1,P2,P3,P4]),
 	unique(Connections),
-	Sol=[P1,P2,P3,P4,P5,C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2,C5I1,C5I2,C6I1,C6I2,O1,O2,O3,O4,O5],
+	Sol=[P1,P2,P3,P4,C1I1,C1I2,C2I1,C2I2,C3I1,C3I2,C4I1,C4I2,O1,O2,O3,O4],
 	labeling([],Sol).
 
 shape10(Connections):-
